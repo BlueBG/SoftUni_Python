a445f651518fcc7f2b9b4dde9259076b9cd7d99e
BlueBG <viktor.stanimirov@gmail.com>
2023-10-08 20:24:40 +0300

    Add more exercises

--- Python DB/PostgreSQL - Exercises/Table Relations - Exercise/03_customs.sql
+++ Python DB/PostgreSQL - Exercises/Table Relations - Exercise/03_customs.sql
@@ -1,27 +1,29 @@
-CREATE TABLE passports(
+CREATE TABLE IF NOT EXISTS passports (
     id INTEGER GENERATED ALWAYS AS IDENTITY (START WITH 100 INCREMENT 1) PRIMARY KEY,
     nationality VARCHAR(50)
-
 );
 
-INSERT INTO passports(nationality)
-    VALUES ('N34FG21B'),
-           ('K65LO4R7'),
-           ('ZE657QP2');
+INSERT INTO
+    passports(nationality)
+VALUES
+    ('N34FG21B'),
+    ('K65LO4R7'),
+    ('ZE657QP2');
 
-CREATE TABLE people(
+CREATE TABLE people (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
-    salary DECIMAL(10,2),
-    pasport_id INTEGER,
+    salary DECIMAL(10, 2),
+    passport_id INT,
 
     CONSTRAINT fk_people_passports
-    FOREIGN KEY (pasport_id)
+    FOREIGN KEY (passport_id)
     REFERENCES passports(id)
-
 );
 
-INSERT INTO people(first_name, salary, pasport_id)
-    VALUES ('Roberto', 43300.0000, 101),
-           ('Tom', 56100.0000, 102),
-           ('Yana', 60200.0000, 100)
+INSERT INTO
+    people (first_name, salary, passport_id)
+VALUES
+    ('Roberto', 43300.00, 101),
+    ('Tom', 56100.00, 102),
+    ('Yana', 60200.00, 100);
