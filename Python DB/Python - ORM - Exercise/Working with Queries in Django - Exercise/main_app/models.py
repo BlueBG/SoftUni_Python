eaf596ec82ca8f6a5a35c9375d6efa94f00703c4
BlueBG <viktor.stanimirov@gmail.com>
2023-11-08 17:42:11 +0200

    Added more exercises

--- Python DB/Python - ORM - Exercise/Working with Queries in Django - Exercise/main_app/models.py
+++ Python DB/Python - ORM - Exercise/Working with Queries in Django - Exercise/main_app/models.py
@@ -79,11 +79,80 @@ class ArtworkGallery(models.Model):
     )
 
 
-# class Laptop(models.Model):
-#     LAPTOP_BRAND_CHOICES = (
-#         ('Asus', 'Asus'),
-#         ('Acer', 'Acer'),
-#         ('Apple', 'Apple'),
-#         ('Lenovo', 'Lenovo'),
-#         ('Dell', 'Dell'),
-#     )
+class Laptop(models.Model):
+    LAPTOP_BRAND_CHOICES = (
+        ('Asus', 'Asus'),
+        ('Acer', 'Acer'),
+        ('Apple', 'Apple'),
+        ('Lenovo', 'Lenovo'),
+        ('Dell', 'Dell'),
+    )
+
+    OPERATION_SYSTEM_CHOICES = (
+        ('Windows', 'Windows'),
+        ('MacOS', 'MacOS'),
+        ('Linux', 'Linux'),
+        ('Chrome OS', 'Chrome OS'),
+    )
+
+    brand = models.CharField(
+        max_length=6,
+        choices=LAPTOP_BRAND_CHOICES,
+    )
+
+    processor = models.CharField(
+        max_length=100,
+    )
+
+    memory = models.PositiveIntegerField(
+        help_text="Memory in GB",
+    )
+
+    storage = models.PositiveIntegerField(
+        help_text="Storage in GB"
+    )
+
+    operation_system = models.CharField(
+        max_length=9,
+        choices=OPERATION_SYSTEM_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
